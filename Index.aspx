fd49e3dfebf3af931c011bf5fa011a431aa675b1
Rafael Jesus <67200847+rafavitorino@users.noreply.github.com>
2025-04-01 14:08:26 -0300

    Subindo projeto

---
+++ Index.aspx
@@ -0,0 +1,107 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="CRUD_Simples_new.Index" EnableEventValidation="false" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <title>Clientes</title>
+
+    <script type="text-javascript">
+        function teste_click()
+        {
+           alert('teste');
+        }
+    </script>
+
+</head>
+<body style="background-color: #F2E3D5;">
+    <form id="frmCadastroCliente" runat="server">
+
+        <div class="container p-5" style="background-color: #012E40 !important; border-radius: 10px; color: white !important; margin-top: 70px;">
+            <h1 class="text-center mb-3" style="color: white;">CADASTRO DE CLIENTES </h1>
+            <!-- BOTÕES -->
+            <div class="row mb-4">
+                <div class="col-6">
+
+                    <button class="btn mt-4 btn-info" title="Novo Registro" id="tsbNovo" name="tsbNovo" style="color: white; font-size: 2em;" runat="server" onserverclick="tsbNovo_Click">
+                        <i class="bi bi-file-earmark-plus-fill"></i>
+                    </button>
+
+                    <button class="btn mt-4 ms-2 btn-success" title="Salvar" id="tsbSalvar" name="tsbSalvar" style="color: white; font-size: 2em;" runat="server" onserverclick="tsbSalvar_Click">
+                        <i class="bi bi-floppy-fill"></i>
+                    </button>
+
+                    <button class="btn mt-4 ms-2 btn-danger" title="Cancelar" id="tsbCancelar" name="tsbCancelar" style="color: white; font-size: 2em;" runat="server" onserverclick="tsbCancelar_Click">
+                        <i class="bi bi-x-square-fill"></i>
+                    </button>
+
+                    <button class="btn mt-4 ms-2 btn-warning" title="Excluir" id="tsbExcluir" name="tsbExcluir" style="color: white; font-size: 2em;" runat="server" onserverclick="tsbExcluir_Click">
+                        <i class="bi bi-trash-fill"></i>
+                    </button>
+                </div>
+                <div class="col-6" runat="server">
+                    <div class="row">
+                        <div class="col-2">
+                            <h5 class="text-center pt-3">Buscar por ID: </h5>
+                        </div>
+                        <div class="col-6 mt-4">
+                            <input class="form-control" name="tsbBuscaPorId" id="txt_bucarId" type="text" runat="server" />
+                        </div>
+                        <div class="col-1" runat="server">
+                            <button class="btn btn-primary mt-4" id="tsbBuscar" name="tsbBuscar" style="font-size: 1em;" runat="server" onserverclick="tsbBuscar_Click"><i class="bi bi-search"></i></button>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- FORMULARIO -->
+            <div class="row mb-4">
+                <div class="col-2">
+                    <asp:Label ID="lbl_ID" runat="server" Text="ID"></asp:Label>
+                    <input id="txtId" type="text" class="form-control" readonly="true" runat="server" />
+                </div>
+                <div class="col-10">
+                    <asp:Label ID="lbl_Nome" runat="server" Text="Nome"></asp:Label>
+                    <input id="txtNome" type="text" class="form-control" maxlength="50" runat="server" />
+                </div>
+            </div>
+
+            <div class="row mb-4">
+
+                <div class="col-md-12">
+                    <asp:Label ID="Label1txtEndereco" runat="server" Text="Endereço"></asp:Label>
+                    <input id="txtEndereco" name="txtEndereco" type="text" class="form-control" maxlength="50" runat="server" />
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="col-2">
+                    <asp:Label ID="lbl_CEP" runat="server" Text="CEP"></asp:Label>
+                    <input id="mskCep" name="mskCep" placeholder="_____-___" type="text" class="form-control" runat="server" />
+                </div>
+                <div class="col-4">
+                    <asp:Label ID="lbl_Bairro" runat="server" Text="Bairro"></asp:Label>
+                    <input id="txtBairro" name="txtBairro" type="text" class="form-control" maxlength="50" runat="server" />
+                </div>
+                <div class="col-4">
+                    <asp:Label ID="lblCidade" runat="server" Text="Cidade"></asp:Label>
+                    <input id="txtCidade" name="txtCidade" type="text" class="form-control" maxlength="50" runat="server" />
+                </div>
+                <div class="col-2">
+                    <asp:Label ID="Label4" runat="server" Text="UF"></asp:Label>
+                    <input id="txtUf" name="txtUf" type="text" class="form-control" maxlength="2" runat="server" />
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="col-2">
+                    <asp:Label ID="lbl_Telefone" runat="server" Text="Telefone"></asp:Label>
+                    <input id="mskTelefone" name="mskTelefone" placeholder="( _ _ ) 9 _ _ _ _ - _ _ _ _" type="tel" class="form-control" runat="server" />
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
+using System.Windows.Forms;
+using System.Configuration;
+
+
+namespace CRUD_Simples_new
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["MinhaConexao"].ConnectionString;
+        static bool novo;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Verifica se essa é a primeira requisição da página, se é uma nova requisição, se não for ele vai direto para o método que queremos
+            if (!IsPostBack)
+            {
+                frmCadastroCliente_Load();
+            }
+
+        }
+
+        // Método da página inicial
+        protected void frmCadastroCliente_Load() //FUNCIONANDO
+        {
+            tsbNovo.Visible = true;
+            tsbSalvar.Visible = false;
+            tsbCancelar.Visible = false;
+            tsbExcluir.Visible = false;
+            txtId.Visible = true;
+            tsbBuscar.Visible = true;
+            txtNome.Disabled = true;
+            txtEndereco.Disabled = true;
+            mskCep.Disabled = true;
+            txtBairro.Disabled = true;
+            txtCidade.Disabled = true;
+            txtUf.Disabled = true;
+            mskTelefone.Disabled = true;
+
+        }
+
+        //Método que ativa os campos e os botões 
+        public void tsbNovo_Click(object sender, EventArgs e) 
+        {       
+            tsbNovo.Visible = false;
+            tsbSalvar.Visible = true;
+            tsbCancelar.Visible = true;
+            tsbExcluir.Visible = false;
+            txtId.Visible = false;
+            tsbBuscar.Visible = false;
+            txtNome.Disabled = false;
+            txtEndereco.Disabled = false;
+            mskCep.Disabled = false;
+            txtBairro.Disabled = false;
+            txtCidade.Disabled = false;
+            txtUf.Disabled = false;
+            mskTelefone.Disabled = false;
+            txtNome.Focus();
+            novo = true;
+        }
+
+        // Método para salvar ou alterar o registro
+        public void tsbSalvar_Click(object sender, EventArgs e) 
+        {
+            if (novo)
+            {
+                string sql = "INSERT INTO CLIENTE (NOME, ENDERECO, CEP, BAIRRO, CIDADE, UF, TELEFONE) VALUES ('" + txtNome.Value + "','" + txtEndereco.Value + "','" + mskCep.Value + "','" + txtBairro.Value + "','" + txtCidade.Value + "','" + txtUf.Value + "','" + mskTelefone.Value + "' );";
+
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+                try
+                {
+                    int i = cmd.ExecuteNonQuery();
+                    if (i > 0)
+                        Response.Write("<script>alert('Cadastrado com sucesso!');</script>");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Erro: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            else
+            {
+                string sql = "UPDATE CLIENTE SET NOME='" + txtNome.Value + "',ENDERECO = '" + txtEndereco.Value + "', " + "CEP='" + mskCep.Value + "', BAIRRO='" + txtBairro.Value + "',CIDADE = '" + txtCidade.Value + "', " + "UF='" + txtUf.Value + "', TELEFONE='" + mskTelefone.Value + "' WHERE ID='" + txt_bucarId.Value + "';";
+
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+                try
+                {
+                    int i = cmd.ExecuteNonQuery();
+                    if (i > 0)
+                        Response.Write("<script>alert('Cadastrado alterado com sucesso!');</script>");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Erro: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+            tsbNovo.Visible = true;
+            tsbSalvar.Visible = false;
+            tsbCancelar.Visible = false;
+            tsbExcluir.Visible = false;
+            txtId.Visible = true;
+            tsbBuscar.Visible = true;
+            txtNome.Disabled = true;
+            txtEndereco.Disabled = true;
+            mskCep.Disabled = true;
+            txtBairro.Disabled = true;
+            txtCidade.Disabled = true;
+            txtUf.Disabled = true;
+            mskTelefone.Disabled = true;
+            txtId.Value = "";
+            txtNome.Value = "";
+            txtEndereco.Value = "";
+            mskCep.Value = "";
+            txtBairro.Value = "";
+            txtCidade.Value = "";
+            txtUf.Value = "";
+            mskTelefone.Value = "";
+
+        }
+        public void tsbCancelar_Click(object sender, EventArgs e) //FUNCIONANDO
+        {
+            tsbNovo.Visible = true;
+            tsbSalvar.Visible = false;
+            tsbCancelar.Visible = false;
+            tsbExcluir.Visible = false;
+            txtId.Visible = true;
+            tsbBuscar.Visible = true;
+            txtNome.Disabled = true;
+            txtEndereco.Disabled = true;
+            mskCep.Disabled = true;
+            txtBairro.Disabled = true;
+            txtCidade.Disabled = true;
+            txtUf.Disabled = true;
+            mskTelefone.Disabled = true;
+            txtId.Value = "";
+            txtNome.Value = "";
+            txtEndereco.Value = "";
+            mskCep.Value = "";
+            txtBairro.Value = "";
+            txtCidade.Value = "";
+            txtUf.Value = "";
+            mskTelefone.Value = "";
+
+        }
+        public void tsbExcluir_Click(object sender, EventArgs e)
+        {
+
+            string sql = "DELETE FROM CLIENTE WHERE ID=" + txt_bucarId.Value;
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                    Response.Write("<script>alert('Registro excluido com sucesso!');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            tsbNovo.Visible = true;
+            tsbSalvar.Visible = false;
+            tsbCancelar.Visible = false;
+            tsbExcluir.Visible = false;
+            txtId.Visible = true;
+            tsbBuscar.Visible = true;
+            txtNome.Disabled = true;
+            txtEndereco.Disabled = true;
+            mskCep.Disabled = true;
+            txtBairro.Disabled = true;
+            txtCidade.Disabled = true;
+            txtUf.Disabled = true;
+            mskTelefone.Disabled = true;
+            txtId.Value = "";
+            txtNome.Value = "";
+            txtEndereco.Value = "";
+            mskCep.Value = "";
+            txtBairro.Value = "";
+            txtCidade.Value = "";
+            txtUf.Value = "";
+            mskTelefone.Value = "";
+        }
+
+        public void tsbBuscar_Click(object sender, EventArgs e) //FUNCIONANDO
+        {
+            string sql = "SELECT * FROM CLIENTE WHERE ID=" + txt_bucarId.Value;
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader;
+            con.Open();
+
+            try
+            {
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    tsbNovo.Visible = false;
+                    tsbSalvar.Visible = true;
+                    tsbCancelar.Visible = true;
+                    tsbExcluir.Visible = true;
+                    txtId.Visible = true;
+                    tsbBuscar.Visible = true;
+                    txtNome.Visible = true;
+                    txtEndereco.Visible = true;
+                    mskCep.Visible = true;
+                    txtBairro.Visible = true;
+                    txtCidade.Visible = true;
+                    txtUf.Visible = true;
+                    mskTelefone.Visible = true;
+                    txtNome.Focus();
+                    txtId.Value = reader[0].ToString();
+                    txtNome.Value = reader[1].ToString();
+                    txtEndereco.Value = reader[2].ToString();
+                    mskCep.Value = reader[3].ToString();
+                    txtBairro.Value = reader[4].ToString();
+                    txtCidade.Value = reader[5].ToString();
+                    txtUf.Value = reader[6].ToString();
+                    mskTelefone.Value = reader[7].ToString();
+                    novo = false;
+                }
+                else
+                    Response.Write("<script>alert('Nenhum registro encontrado com o ID especificado');</script>");
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            txtId.Value = "";
+
+        }
+
+    }
+
+}
+                     
+
+
+
+
